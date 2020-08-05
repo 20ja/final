f968e7f2af8ef133a3cb7087e101fe5c88b8a837
20ja <61406318+20ja@users.noreply.github.com>
2020-08-06 01:02:21 +0800

    debug

--- db.js
+++ db.js
@@ -4,53 +4,121 @@ import beautifyUnique from 'mongoose-beautiful-unique-validation'
 
 dotenv.config()
 
+mongoose.set('useCreateIndex', true)
+mongoose.set('useUnifiedTopology', true)
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useFindAndModify', false)
+
 const Schema = mongoose.Schema
-// 連到資料庫，設定在env環境檔裡
 mongoose.connect(process.env.DBURL)
 mongoose.plugin(beautifyUnique)
 
+// 會員模板
 const userSchema = new Schema({
+  name: {
+    type: String,
+    minlength: [2, '姓名最少2個字以上'],
+    required: [true, '請輸入姓名']
+  },
   account: {
     type: String,
-    minlength: [4, '帳號必須 4 個字以上'],
-    maxlength: [20, '帳號必須 20 個字以下'],
+    minlength: [6, '帳號必須6個字以上'],
+    maxlength: [20, '帳號必須二十個字以下'],
     unique: '帳號已使用',
     required: [true, '請輸入帳號']
   },
   password: {
     type: String,
     required: [true, '請輸入密碼']
+  },
+  email: {
+    type: String,
+    required: [true, '請輸入email']
   }
 }, {
   versionKey: false
 })
 
-const fileSchema = new Schema({
-  user: {
+// 商品模板
+const productSchema = new Schema({
+  name: {
     type: String,
-    required: [true, '請輸入使用者名稱']
+    required: [true, '需要商品圖片']
+  },
+  title: {
+    type: String,
+    required: [true, '需要商品名稱']
+  },
+  price: {
+    type: Number,
+    required: [true, '需要商品價格']
+  },
+  originalprice: {
+    type: String,
+    required: [true, '需要商品定價']
+  },
+  ISBN: {
+    type: String,
+    required: [true, '需要商品編碼']
+  },
+  preview: {
+    type: Array,
+    required: [true, '需要商品預覽圖']
   },
   description: {
     type: String,
-    maxlength: [200, '文字必須為200字以下']
+    required: [true, '需要商品介紹']
   },
-  name: {
+  category: {
     type: String,
-    required: [true, '沒有檔案名稱']
+    required: [true, '需要商品分類']
   }
 }, {
   versionKey: false
 })
 
-// process.env.COLLECTION_USER =>'users'
-// process.env.COLLECTION_FILE =>'files'
-// 連到 env 檔
+// 購物車模板
+const cartSchema = new Schema({
+  id: {
+    type: String,
+    required: [true, '需要使用者ID']
+  },
+  product: {
+    type: Object,
+    required: [true, '需要商品明細']
+  }
+}, {
+  versionKey: false
+})
+
+// 訂單模板
+const orderSchema = new Schema({
+  id: {
+    type: String,
+    required: [true, '需要使用者ID']
+  },
+  cart: {
+    type: Array,
+    required: [true, '需要訂單內容']
+  },
+  totalprice: {
+    type: Number,
+    required: [true, '需要總金額']
+  }
+}, {
+  versionKey: false
+})
+
 const users = mongoose.model(process.env.COLLECTION_USER, userSchema)
-const files = mongoose.model(process.env.COLLECTION_FILE, fileSchema)
+const products = mongoose.model(process.env.COLLECTION_PRODUCT, productSchema)
+const carts = mongoose.model(process.env.COLLECTION_CART, cartSchema)
+const orders = mongoose.model(process.env.COLLECTION_ORDER, orderSchema)
 const connection = mongoose.connection
 
 export default {
   users,
-  files,
+  products,
+  carts,
+  orders,
   connection
 }
--- index.js
+++ index.js
@@ -8,10 +8,11 @@ import md5 from 'md5'
 import dotenv from 'dotenv'
 import path from 'path'
 import FTPStorage from 'multer-ftp'
-// 需寫入檔案時要用到的(內建套件)
 import fs from 'fs'
-
+import uuidv4 from 'uuidv4'
 import db from './db.js'
+
+const uuid = uuidv4.uuid
 
 dotenv.config()
 
@@ -22,40 +23,26 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors({
   origin (origin, callback) {
-    // 直接開網頁，不是 ajax 時，origin 是 undefined
-    if (origin === undefined) {
+    if (process.env.ALLOW_CORS === 'true') {
+      // 開發環境，允許
+      callback(null, true)
+    } else if (origin.includes('github')) {
+      // 非開發環境，但是從 github 過來，允許
       callback(null, true)
     } else {
-      if (process.env.ALLOW_CORS === 'true') {
-        // 開發環境，允許
-        callback(null, true)
-      } else if (origin.includes('github')) {
-        // 非開發環境，但是從 github 過來，允許
-        callback(null, true)
-      } else {
-        // 不是開發也不是從 github 過來，拒絕
-        callback(new Error('Not allowed'), false)
-      }
+      // 不是開發也不是從 github 過來，拒絕
+      callback(new Error('Not allowed'), false)
     }
   },
-  // 取得認證資訊
   credentials: true
 }))
-// *拿來加密的訊號
 app.use(session({
-  secret: 'album',
-  // 將 session 存入 mongodb
+  secret: '20200713 fin',
   store: new MongoStore({
-    // 使用 mongoose 的資料庫連接
     mongooseConnection: db.connection,
-    // 設定存入的 collection
     collection: process.env.COLLECTION_SESSION
   }),
-  // *session 有效期間
   cookie: {
-    // 1000 毫秒 = 一秒鐘
-    // 1000 毫秒 * 60 = 一分鐘
-    // 1000 毫秒 * 60 * 30 = 三十分鐘
     maxAge: 1000 * 60 * 30
   },
   // 是否保存未修改的 session
@@ -64,7 +51,6 @@ app.use(session({
   rolling: true
 }))
 
-// *上傳設定
 let storage
 if (process.env.FTP === 'false') {
   // 開發環境將上傳檔案放本機
@@ -73,7 +59,8 @@ if (process.env.FTP === 'false') {
       cb(null, 'images/')
     },
     filename (req, file, cb) {
-      cb(null, Date.now() + path.extname(file.originalname))
+      // cb(null, Date.now() + path.extname(file.originalname))
+      cb(null, uuid() + path.extname(file.originalname))
     }
   })
 } else {
@@ -81,7 +68,7 @@ if (process.env.FTP === 'false') {
   storage = new FTPStorage({
     // 上傳伺服器的路徑
     basepath: '/',
-    // FTP 設定 multer-ftp 寫法
+    // FTP 設定
     ftp: {
       host: process.env.FTP_HOST,
       secure: false,
@@ -106,54 +93,50 @@ const upload = multer({
     }
   },
   limits: {
-    // 1024*1024=1MB
     fileSize: 1024 * 1024
   }
 })
 
-// *監聽
 app.listen(process.env.PORT, () => {
-  console.log(('已啟動'))
+  console.log('已啟動')
 })
 
-// *註冊
+// 註冊
 app.post('/users', async (req, res) => {
-  if (!req.headers['content-type'].includes('application/json')) {
-    res.status(400)
-    res.send({ success: false, message: '格式不符' })
-  }
-
-  try {
-    await db.users.create({
-      account: req.body.account,
-      password: md5(req.body.password)
-    })
-    res.status(200)
-    res.send({ success: true, message: `${req.body.account} 註冊成功` })
-  } catch (error) {
-    console.log(error)
-    // 資料格式錯誤
-    if (error.name === 'ValidationError') {
-      const key = Object.keys(error.errors)[0]
-      const message = error.errors[key].message
-      res.status(400)
-      res.send({ success: false, message })
-    } else {
-      // 伺服器錯誤(程式問題)
-      res.status(500)
-      res.send({ success: false, message: '伺服器錯誤' })
-    }
-  }
-})
-
-// *登入
-app.post('/login', async (req, res) => {
   if (!req.headers['content-type'].includes('application/json')) {
     res.status(400)
     res.send({ success: false, message: '格式不符' })
     return
   }
 
+  try {
+    await db.users.create({
+      name: req.body.name,
+      account: req.body.account,
+      password: md5(req.body.password),
+      email: req.body.email
+    })
+    res.send({ success: true, message: '' })
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(400)
+      res.send({ success: false, message })
+    } else {
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+})
+
+// 登入
+app.post('/login', async (req, res) => {
+  if (!req.headers['content-type'].includes('application/json')) {
+    res.status(400)
+    res.send({ success: false, message: '格式不符' })
+    return
+  }
   try {
     const result = await db.users.find(
       {
@@ -165,7 +148,7 @@ app.post('/login', async (req, res) => {
     if (result.length > 0) {
       req.session.user = result[0].account
       res.status(200)
-      res.send({ success: true, message: `${req.body.account} 登入成功` })
+      res.send({ success: true, message: '', result })
     } else {
       res.status(404)
       res.send({ success: false, message: '帳號密碼錯誤' })
@@ -181,11 +164,12 @@ app.post('/login', async (req, res) => {
       // 伺服器錯誤
       res.status(500)
       res.send({ success: false, message: '伺服器錯誤' })
+      console.log(error)
     }
   }
 })
 
-// *登出
+// 登出
 app.delete('/logout', async (req, res) => {
   req.session.destroy(error => {
     if (error) {
@@ -199,72 +183,59 @@ app.delete('/logout', async (req, res) => {
   })
 })
 
-// *知道使用者狀態 (維持住 session 登入，因為 session 每隔30分鐘過期的狀態)
-app.get('/heartbeat', async (req, res) => {
-  let islogin = false
-  if (req.session.user !== undefined) {
-    islogin = true
-  }
-  res.status(200)
-  res.send(islogin)
-  // 或是可以這樣寫↓
-  // res.send(req.session.user !== undefined)
-})
-
-// *上傳檔案
-app.post('/file', async (req, res) => {
-  // 沒有登入
+// 上架
+app.post('/upload', async (req, res) => {
   if (req.session.user === undefined) {
     res.status(401)
     res.send({ success: false, message: '未登入' })
     return
   }
   if (!req.headers['content-type'].includes('multipart/form-data')) {
-    // 'multipart(有包含檔案)/form-data( html 的 form 標籤)'
     res.status(400)
     res.send({ success: false, message: '格式不符' })
     return
   }
-
-  // 有一個上傳進來的檔案，欄位是 image
-  // req，進來的東西
-  // res，要出去的東西
-  // err，檔案上傳的錯誤
-  // upload.single(欄位)(req, res, 上傳完畢的 function)
-  // 多個檔案的話改 array()
-  upload.single('image')(req, res, async error => {
+  upload.array('image')(req, res, async error => {
     if (error instanceof multer.MulterError) {
       // 上傳錯誤
       let message = ''
       if (error.code === 'LIMIT_FILE_SIZE') {
         message = '檔案太大'
       } else {
-        message = '檔案格式不符'
+        message = '格式不符'
       }
       res.status(400)
       res.send({ success: false, message })
     } else if (error) {
       res.status(500)
-      // console.log(error)
-      // 自己要創建 images 資料夾
-      res.send({ success: false, message: '1伺服器錯誤' })
+      res.send({ success: false, message: '伺服器錯誤' })
     } else {
       try {
-        let name = ''
+        const name = []
         if (process.env.FTP === 'true') {
-          name = path.basename(req.file.path)
+          for (const i of req.files) {
+            name.push(path.basename(i.path))
+          }
         } else {
-          name = req.file.filename
+          for (const i of req.files) {
+            name.push(path.basename(i.path))
+          }
         }
-        const result = await db.files.create(
+        const cover = name[0]
+        name.shift()
+        const result = await db.products.create(
           {
-            user: req.session.user,
+            name: cover,
+            title: req.body.title,
+            price: req.body.price,
+            originalprice: req.body.originalprice,
+            ISBN: req.body.ISBN,
             description: req.body.description,
-            name
+            preview: name,
+            category: req.body.category
           }
         )
-        res.status(200)
-        res.send({ success: true, message: '', name, _id: result._id })
+        res.send({ success: true, message: '', result })
       } catch (error) {
         if (error.name === 'ValidationError') {
           // 資料格式錯誤
@@ -273,23 +244,28 @@ app.post('/file', async (req, res) => {
           res.status(400)
           res.send({ success: false, message })
         } else {
+          console.log(error)
           // 伺服器錯誤
           res.status(500)
-          res.send({ success: false, message: '2伺服器錯誤' })
+          res.send({ success: false, message: '伺服器錯誤' })
         }
       }
     }
   })
 })
 
-// *提取圖檔 範例：http://localhost:3000/file/1593412482049.jpg
-app.get('/file/:name', async (req, res) => {
-  // 要登入的限制
-  if (req.session.user === undefined) {
-    res.status(401)
-    res.send({ success: false, message: '未登入' })
-    return
+// 查看所有書
+app.get('/upload', async (req, res) => {
+  try {
+    const result = await db.products.find()
+    res.send({ success: true, message: '', result })
+  } catch (error) {
+    res.status(500).send({ success: false, message: '伺服器錯誤' })
   }
+})
+
+// 取得書本的圖片
+app.get('/upload/:name', async (req, res) => {
   if (process.env.FTP === 'false') {
     const path = process.cwd() + '/images/' + req.params.name
     const exists = fs.existsSync(path)
@@ -305,58 +281,15 @@ app.get('/file/:name', async (req, res) => {
   }
 })
 
-// *個人相簿的所有檔案
-app.get('/album/:user', async (req, res) => {
+// 刪除商品
+app.delete('/upload/:id', async (req, res) => {
   if (req.session.user === undefined) {
-    // 必須要登入
     res.status(401)
     res.send({ success: false, message: '未登入' })
     return
   }
-  if (req.session.user !== req.params.user) {
-    // 已登入，但無權限訪問
-    res.status(401)
-    res.send({ success: false, message: '無權限' })
-  }
-
   try {
-    // 撈此使用者的所有資料
-    const result = await db.files.find({ user: req.params.user })
-    res.status(200)
-    res.send({ success: true, message: '', result })
-  } catch (error) {
-    res.status(500)
-    res.send({ success: false, message: '伺服器錯誤' })
-  }
-})
-
-// *更新相片標題敘述
-// patch 只更新文字，若是用 push 是全部種類都可以(含圖片)
-app.patch('/file/:id', async (req, res) => {
-  if (!req.headers['content-type'].includes('application/json')) {
-    res.status(400)
-    res.send({ success: false, message: '格式不符' })
-    return
-  }
-  // 沒有登入
-  if (!req.session.user) {
-    res.status(401)
-    res.send({ success: false, message: '無權限' })
-    return
-  }
-
-  try {
-    // 檢查相片擁有者是否為本人
-    let result = await db.files.findById(req.params.id)
-    if (result.user !== req.session.user) {
-      res.status(401)
-      res.send({ success: false, message: '無權限' })
-      return
-    }
-    // findByIdAndUpdate 預設回傳的是更新前的資料
-    // 設定 new true 後會變成回傳新的資料
-    result = await db.files.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    res.status(200)
+    const result = await db.products.findByIdAndRemove(req.params.id)
     res.send({ success: true, message: '', result })
   } catch (error) {
     if (error.name === 'CastError') {
@@ -377,24 +310,64 @@ app.patch('/file/:id', async (req, res) => {
   }
 })
 
-// *刪除資料
-app.delete('/file/:id', async (req, res) => {
-  // 沒有登入
-  if (!req.session.user) {
-    res.status(401)
-    res.send({ success: false, message: '無權限' })
+// 新增購物車
+app.post('/cart', async (req, res) => {
+  if (!req.headers['content-type'].includes('application/json')) {
+    res.status(400)
+    res.send({ success: false, message: '格式不符' })
     return
   }
-
+  if (req.session.user === undefined) {
+    res.status(401)
+    res.send({ success: false, message: '未登入' })
+    return
+  }
   try {
-    // 檢查相片擁有者是不是本人
-    let result = await db.files.findById(req.params.id)
-    if (result.user !== req.session.user) {
-      res.status(403)
-      res.send({ success: false, message: '無權限' })
-      return
+    const result = await db.carts.create({
+      id: req.body.id,
+      product: req.body.product
+    })
+    res.send({ success: true, message: '', result })
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      // 資料格式錯誤
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(400)
+      res.send({ success: false, message })
+    } else {
+      console.log(error)
+      // 伺服器錯誤
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
     }
-    result = await db.files.findByIdAndDelete(req.params.id)
+  }
+})
+
+// 取得個人購物車資訊
+app.get('/cart/:id', async (req, res) => {
+  if (req.session.user === undefined) {
+    res.status(401)
+    res.send({ success: false, message: '未登入' })
+    return
+  }
+  try {
+    const result = await db.carts.find({ id: req.params.id })
+    res.send({ success: true, message: '', result })
+  } catch (error) {
+    res.send({ success: false, message: '伺服器錯誤' })
+  }
+})
+
+// 購物車刪除
+app.delete('/cart/:id', async (req, res) => {
+  if (req.session.user === undefined) {
+    res.status(401)
+    res.send({ success: false, message: '未登入' })
+    return
+  }
+  try {
+    const result = await db.carts.findByIdAndDelete(req.params.id)
     if (result === null) {
       res.status(404)
       res.send({ success: true, message: '找不到資料' })
@@ -413,4 +386,86 @@ app.delete('/file/:id', async (req, res) => {
       res.send({ success: false, message: '伺服器錯誤' })
     }
   }
+})
+
+// 送出訂單
+app.post('/order', async (req, res) => {
+  if (req.session.user === undefined) {
+    res.status(401)
+    res.send({ success: false, message: '未登入' })
+    return
+  }
+  if (!req.headers['content-type'].includes('application/json')) {
+    res.status(400)
+    res.send({ success: false, message: '格式不符' })
+    return
+  }
+  try {
+    // 新增訂單，使用者的ID以及訂單內容
+    const result = await db.orders.create({
+      id: req.body.id,
+      cart: req.body.carts,
+      totalprice: req.body.totalprice
+    })
+    // 同時刪除該使用者購物車的所有資料
+    await db.carts.remove({ id: req.body.id })
+    res.send({ success: true, message: '', result })
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      // 資料格式錯誤
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(400)
+      res.send({ success: false, message })
+    } else {
+      console.log(error)
+      // 伺服器錯誤
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+})
+
+// 查詢所有訂單
+app.get('/order', async (req, res) => {
+  if (req.session.user === undefined) {
+    res.status(401)
+    res.send({ success: false, message: '未登入' })
+    return
+  }
+  try {
+    const result = await db.orders.find()
+    for (const i of result) {
+      let username = await db.users.findById(i.id)
+      username = username.name
+      i.id = username
+    }
+    res.send({ success: true, message: '', result })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ success: false, message: '伺服器錯誤' })
+  }
+})
+
+// 查詢書籍
+app.get('/search/:title', async (req, res) => {
+  try {
+    const result = await db.products.find({ title: { $regex: req.params.title } })
+    console.log(result)
+    res.send({ success: true, message: '', result })
+  } catch (error) {
+    res.status(500).send({ success: false, message: '伺服器錯誤' })
+  }
+})
+
+// *知道使用者狀態 (維持住 session 登入，因為 session 每隔30分鐘過期的狀態)
+app.get('/heartbeat', async (req, res) => {
+  let islogin = false
+  if (req.session.user !== undefined) {
+    islogin = true
+  }
+  res.status(200)
+  res.send(islogin)
+  // 或是可以這樣寫↓
+  // res.send(req.session.user !== undefined)
 })
