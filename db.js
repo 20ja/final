21f108d79a0e244ec457cdec2d9d5df479eaea2a
ja <z91030120@gmail.com>
2020-08-06 00:42:09 +0800

    修改設定 ＋新增heart

--- db.js
+++ db.js
@@ -5,14 +5,15 @@ import beautifyUnique from 'mongoose-beautiful-unique-validation'
 dotenv.config()
 
 const Schema = mongoose.Schema
+// 連到資料庫，設定在env環境檔裡
 mongoose.connect(process.env.DBURL)
 mongoose.plugin(beautifyUnique)
 
 const userSchema = new Schema({
   account: {
     type: String,
-    minlength: [4, '帳號必須四個字以上'],
-    maxlength: [20, '帳號必須二十個字以下'],
+    minlength: [4, '帳號必須 4 個字以上'],
+    maxlength: [20, '帳號必須 20 個字以下'],
     unique: '帳號已使用',
     required: [true, '請輸入帳號']
   },
@@ -27,11 +28,11 @@ const userSchema = new Schema({
 const fileSchema = new Schema({
   user: {
     type: String,
-    required: [true, '沒有使用者名稱']
+    required: [true, '請輸入使用者名稱']
   },
   description: {
     type: String,
-    maxlength: [200, '帳號必須二十個字以下']
+    maxlength: [200, '文字必須為200字以下']
   },
   name: {
     type: String,
@@ -41,6 +42,9 @@ const fileSchema = new Schema({
   versionKey: false
 })
 
+// process.env.COLLECTION_USER =>'users'
+// process.env.COLLECTION_FILE =>'files'
+// 連到 env 檔
 const users = mongoose.model(process.env.COLLECTION_USER, userSchema)
 const files = mongoose.model(process.env.COLLECTION_FILE, fileSchema)
 const connection = mongoose.connection
